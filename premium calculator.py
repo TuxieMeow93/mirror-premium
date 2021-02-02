0c5a5a3482afebca53891a7de0255074a0ff8232
dev-bhaskar8 <43923435+dev-bhaskar8@users.noreply.github.com>
2021-02-03 04:19:11 +0530

    Add files via upload

---
+++ premium calculator.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import re
+
+query = """{
+  query: 
+   assets { symbol prices{
+    price
+    oraclePrice
+  	}
+  }
+}"""
+url = 'https://graph.mirror.finance/graphql'
+r = requests.post(url,json={'query': query})
+
+symbol = 'm[A-Z]{3,4}'
+price = 'price":"\d+.\d+'
+oraclePrice = 'Price":"\d+.\d'
+innerPrice = '\d+.\d+'
+
+symbols = re.findall(symbol, r.text)
+prices = re.findall(price, r.text)
+del prices[1]
+oraclePrices = re.findall(oraclePrice, r.text)
+
+for i in range(0,len(symbols)):
+       
+       u=float(re.findall(innerPrice,prices[i])[0])
+       l=float(re.findall(innerPrice,oraclePrices[i])[0])
+       premium=((abs(u-l))/l)*100
+       print(symbols[i],"|","Price : ",u,"|","Oracle Price : ",l,"|","Premium : ",str(premium)[:5],"%")
+
+
+       
+
