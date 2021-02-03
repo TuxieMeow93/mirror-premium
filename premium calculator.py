10293ff80fe22379220f95f9562c9ba5373657ed
dev-bhaskar8 <43923435+dev-bhaskar8@users.noreply.github.com>
2021-02-03 08:10:33 +0530

    Update premium calculator.py

--- premium calculator.py
+++ premium calculator.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import re
-
 query = """{
   query: 
    assets { symbol prices{
@@ -12,23 +11,21 @@ query = """{
 }"""
 url = 'https://graph.mirror.finance/graphql'
 r = requests.post(url,json={'query': query})
-
-symbol = 'm[A-Z]{3,4}'
+symbol = 'm[A-Z]{3,4}|MIR'
 price = 'price":"\d+.\d+'
 oraclePrice = 'Price":"\d+.\d'
 innerPrice = '\d+.\d+'
-
 symbols = re.findall(symbol, r.text)
+ix=symbols.index('MIR')
 prices = re.findall(price, r.text)
-del prices[1]
 oraclePrices = re.findall(oraclePrice, r.text)
-
+oraclePrices.insert(ix, f'''Price":"{prices[ix]}''')
 for i in range(0,len(symbols)):
-       
        u=float(re.findall(innerPrice,prices[i])[0])
        l=float(re.findall(innerPrice,oraclePrices[i])[0])
        premium=((abs(u-l))/l)*100
-       print(symbols[i],"|","Price : ",str(u)[:5],"|","Oracle Price : ",str(l)[:5],"|","Premium : ",str(premium)[:5],"%")
+       print(symbols[i],"\n","Price : ",str(u)[:5],"|","Oracle Price : ",str(l)[:5],"|","Premium : ",str(premium)[:5],"%")
+       print("-----------------------------------------------------------")
 
 
        
