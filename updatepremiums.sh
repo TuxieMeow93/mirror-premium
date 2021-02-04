1702451f20fcb97b05e71a735cc1ebf669a751f3
TuxieMeow93 <mossme89@yahoo.com>
2021-02-04 11:50:38 -0500

    Update updatepremiums.sh

--- updatepremiums.sh
+++ updatepremiums.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+newline()
+{
+echo "----------" | tee -a /var/www/html/sortedmirrorpremiums_$today.txt
+}
+
+
 cd /root
 
 dt=$(date +%Y-%m-%d_%H_%M)
@@ -12,16 +19,17 @@ cd /var/www/html
 cp mirrorpremiums/$today/$today.txt mirrorpremiums_$today.txt
 
 ##search for individual assets
-grep -ie "mQQQ" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mQQQmirrorpremiums_$today.txt
-grep -ie "mTSLA" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mTSLAmirrorpremiums_$today.txt
-grep -ie "mNFLX" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mNFLXmirrorpremiums_$today.txt
-grep -ie "mIAU" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mIAUmirrorpremiums_$today.txt
-grep -ie "mUSO" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mUSOmirrorpremiums_$today.txt
-grep -ie "mSLV" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mSLVmirrorpremiums_$today.txt
-grep -ie "mTWTR" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mTWTRmirrorpremiums_$today.txt
-grep -ie "mVIXY" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mVIXYmirrorpremiums_$today.txt
-grep -ie "mAMZN" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mAMZNmirrorpremiums_$today.txt
-grep -ie "mGOOG" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mGOOGmirrorpremiums_$today.txt
-grep -ie "mMSFT" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mMSFTmirrorpremiums_$today.txt
-grep -ie "mBABA" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mBABAmirrorpremiums_$today.txt
-grep -ie "mAAPL" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mAAPLmirrorpremiums_$today.txt
+grep -ie "mQQQ" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mQQQmirrorpremiums_$today.txt | tee -a sortedmirrorpremiums_$today.txt
+newline()
+grep -ie "mTSLA" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mTSLAmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mNFLX" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mNFLXmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mIAU" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mIAUmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mUSO" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mUSOmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mSLV" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mSLVmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mTWTR" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mTWTRmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mVIXY" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mVIXYmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mAMZN" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mAMZNmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mGOOG" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mGOOGmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mMSFT" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mMSFTmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mBABA" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mBABAmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
+grep -ie "mAAPL" /var/www/html/mirrorpremiums_$today.txt | tee /var/www/html/mAAPLmirrorpremiums_$today.txt| tee -a sortedmirrorpremiums_$today.txt
