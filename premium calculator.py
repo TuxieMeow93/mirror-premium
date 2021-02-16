65edf44735c16665a6efbb58bf7ba08a72706b17
TuxieMeow93 <mossme89@yahoo.com>
2021-02-16 08:12:27 -0500

    v2.1
    Added APR and other metrics. All credit goes to Discord user somethingelse

--- premium calculator.py
+++ premium calculator.py
@@ -9,6 +9,11 @@ query = """{
     price
     oraclePrice
         }
+    statistic{
+    volume 
+    liquidity
+    apr
+        }
   }
 }"""
 
@@ -27,11 +32,25 @@ df = pd.DataFrame(data1)
 df = pd.json_normalize(data1["data"]["query"])
 
 #Rename Colums
-df.rename(columns = {'symbol':'Symbol', 'prices.price':'Price', 'prices.oraclePrice':'Oracle Price'}, inplace = True)
+df.rename(columns = {
+    'symbol':'Symbol', 
+    'prices.price':'Price', 
+    'prices.oraclePrice':'Oracle Price',
+    'statistic.volume':'Volume',
+    'statistic.liquidity':'Liquidity', 
+    'statistic.apr':'APR'}, 
+    
+    inplace = True
+    )
 
 #Format Data Types
 df['Oracle Price'] = pd.to_numeric(df['Oracle Price'], errors = 'coerce')
 df['Price'] = pd.to_numeric(df['Price'], errors = 'coerce')
+df['Volume'] = pd.to_numeric(df['Volume'], errors = 'coerce')
+df['Liquidity'] = pd.to_numeric(df['Liquidity'], errors = 'coerce')
+df['Volume'] = df['Volume']/1000000
+df['Liquidity'] = df['Liquidity']/1000000
+df['APR'] = pd.to_numeric(df['APR'], errors = 'coerce')
 
 #Additional Columns & Calculations
 Premium = df['Price'] - df['Oracle Price']
@@ -53,11 +72,13 @@ df = df[first_cols+last_cols]
 df['Price'] = df['Price'].map("${:,.2f}".format)
 df['Oracle Price'] = df['Oracle Price'].map("${:,.2f}".format)
 df['Premium'] = df['Premium'].map("{:.2%}".format)
+df['Volume'] = (df['Volume'].astype(float)/1000000).round(2).astype(str) + ' M'
+df['Liquidity'] = (df['Liquidity'].astype(float)/1000000).round(2).astype(str) + ' M'
+df['APR'] = df['APR'].map("{:.2%}".format)
 
 #See Ya!
-df.to_csv("list1.csv")
+df.to_csv("list1.csv", index = False)
 df_pretty = df.to_string(index = False)
-
 
 print(df_pretty)
 
